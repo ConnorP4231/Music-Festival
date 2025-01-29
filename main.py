d023e85bad6afc15f0f74cf8b847438791e8ca6c
ConnorP4231 <connor.pavicic@ucas-edu.net>
2025-01-29 13:40:42 -0700

    worked on some code but don't know it it's correct

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def management():
     three_day = 0
     one_day_VIP = 0
     three_day_VIP = 0
+    attendee_names = []
 
     choice = input("""
 Options:
@@ -15,8 +16,9 @@ Options:
 1. Add tickets to total tickets
 2. Remove tickets from total tickets
 3. Check attendees
-4. Check ticket sales
-5. Check ticket profits
+4. Add a name to attendees
+5. Check ticket sales
+6. Check ticket profits
 
 Answer (1, 2, 3, 4, 5): """)
     
@@ -30,9 +32,35 @@ Answer (1, 2, 3, 4, 5): """)
 Answer (1, 2, 3, 4): """)
             
             if typetickets == '1':
-                cost = input('How much do one of those tickets cost?: ')
-                num_tickets = input('How many of those tickets did you sell?: ')
-                money = int(cost)*int(num_tickets)
-                print(f'Addition Successfull! (You made ${money}).')
+                while True:
+                    try:
+                        cost = input('How much do one of those tickets cost?: ')
+                        num_tickets = input('How many of those tickets did you sell?: ')
+                        break
+                    except ValueError:
+                        print('Type a number.')
+                    money = int(cost)*int(num_tickets)
+                    print(f'Addition Successful! (You made ${money}).')
+            elif typetickets == '2':
+                while True:
+                    try:
+                        cost = input('How much do one of those tickets cost?: ')
+                        num_tickets = input('How many of those tickets did you sell?: ')
+                        break
+                    except ValueError:
+                        print('Type a number.')
+                    money = int(cost)*int(num_tickets)
+                    print(f'Addition Successful! (You made ${money}).')
+            elif typetickets == '3':
+                while True:
+                    try:
+                        cost = input('How much do one of those tickets cost?: ')
+                        num_tickets = input('How many of those tickets did you sell?: ')
+                        break
+                    except ValueError:
+                        print('Type a number.')
+                    money = int(cost)*int(num_tickets)
+                    print(f'Addition Successful! (You made ${money}).')
+
 
 management()
