b808e05de91d77993261c55fdac30fdc248e9c86
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-01-30 13:44:05 -0700

    Fairus

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+#Fairus Aritst managment
+#this project will use sets
 
+
+#functin to show artist
+def artist():
+    thisset = {"artist a", "artist b", "artist c"}
+    for x in thisset:
+        print(x)
+
+def genre():
+    pass
+
+def main():
+    input("find artist change artish remove artist: ")
+    print(input)
