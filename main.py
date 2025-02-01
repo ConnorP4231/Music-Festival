fcb1d9deae3769aa19da280521825a136845684d
UCAS-John <john.wangwang@ucas-edu.net>
2025-02-01 02:40:34 +0000

    improve UI

--- main.py
+++ main.py
@@ -1,75 +1,75 @@
 def add_venue(schedule: dict, venues: dict, venue_name: str):
     schedule[venue_name] = {}
     venues[venue_name] = {}
-    print(f"Successfully add {venue_name} to venues")
+    print(f"\nSuccessfully add {venue_name} to venues")
     return True
 
 def remove_venue(schedule: dict, venues: dict, venue_name):
     if venue_name in venues:
         del schedule[venue_name]
         del venues[venue_name]
-        print("Venue removed successfully")
+        print("\nVenue removed successfully")
     else:
-        print("Error: Venue does not exist")
+        print("\nError: Venue does not exist")
 
 def add_stage(schedule: dict, venues: dict, venue_name: str, stage_name: str, location: str):
     if venue_name in venues:
         if stage_name in venues[venue_name]:
-            print("Error: Stage already exist")
+            print("\nError: Stage already exist")
             return False
         else:
             schedule[venue_name][stage_name] = []
             venues[venue_name][stage_name] = {"location": location, "equipment": set()}
-            print(f"Successfully add {stage_name} to {venue_name}")
+            print(f"\nSuccessfully add {stage_name} to {venue_name}")
             return True
     else:
-        print("Error: Venue does not exist")
+        print("\nError: Venue does not exist")
         return False
         
 def remove_stage(schedule: dict, venues: dict, venue_name: str, stage_name: str):
     if venue_name in venues and stage_name in venues[venue_name]:
         del schedule[venue_name][stage_name]
         del venues[venue_name][stage_name]
-        print(f"Succesfully remove {stage_name} from {venue_name}")
+        print(f"\nSuccesfully remove {stage_name} from {venue_name}")
         return True
     else:
-        print("Error: Venue or stage does not exist")
+        print("\nError: Venue or stage does not exist")
         return False
     
 def change_location(venues: dict, venue_name: str, stage_name: str, location: str):
     if venue_name in venues and stage_name in venues[venue_name]:
         venues[venue_name][stage_name]["location"] = location
-        print(f"Succesfully change location to {location}")
+        print(f"\nSuccesfully change location to {location}")
         return True
     else:
-        print("Error: Venue or stage does not exist")
+        print("\nError: Venue or stage does not exist")
         return False
 
 def change_equipment(venues: dict, venue_name: str, stage_name: str, equipment_items: set):
     if venue_name in venues and stage_name in venues[venue_name]:
         venues[venue_name][stage_name]["equipment"] = equipment_items
-        print(f"Succesfully set equipment to {equipment_items}")
+        print(f"\nSuccesfully set equipment to {equipment_items}")
         return True
     else:
-        print("Error: Venue or stage does not exist")
+        print("\nError: Venue or stage does not exist")
         return False
 
 def assign_artist_schedule(time_slot_list: tuple, schedule: dict, venue_name: str, stage_name: str, time_slot: str, artist_name: str):
     if time_slot not in time_slot_list:
-        print("Please select appropiate time slot provided")
+        print("\nPlease select appropiate time slot provided")
         return False
     
     if venue_name in schedule and stage_name in schedule[venue_name]:
         for (time, artist) in schedule[venue_name][stage_name]:
             if time_slot == time:
-                print("Time slot already taken")
+                print("\nTime slot already taken")
                 return False
             else:
                 schedule[venue_name][stage_name].append((time_slot, artist_name))
-                print(f"Successfully add artiist {artist_name} in time {time_slot}")
+                print(f"\nSuccessfully add artiist {artist_name} in time {time_slot}")
                 return True
     else:
-        print("Error: Venue or stage does not exist")
+        print("\nError: Venue or stage does not exist")
         return False
     
 def remove_time_slot(schedule: dict, venue_name: str, stage_name:str, time_slot: str):
@@ -78,10 +78,10 @@ def remove_time_slot(schedule: dict, venue_name: str, stage_name:str, time_slot:
             print("You didn't assign artist in this time slot!")
     if venue_name in schedule and stage_name in schedule[venue_name]:
         schedule[venue_name][stage_name] = [(time, artist) for time, artist in schedule[venue_name][stage_name] if time != time_slot]
-        print(f"Successfully remove time slot {time_slot} from {stage_name} in {venue_name}")
+        print(f"\nSuccessfully remove time slot {time_slot} from {stage_name} in {venue_name}")
         return True
     else:
-        print("Error: Venue or stage does not exist")
+        print("\nError: Venue or stage does not exist")
         return False
 
 def change_artist(schedule: dict, venue_name: str, stage_name: str, time_slot: str, artist_name: str):
@@ -91,13 +91,14 @@ def change_artist(schedule: dict, venue_name: str, stage_name: str, time_slot: s
     if venue_name in schedule and stage_name in schedule[venue_name]:
         schedule[venue_name][stage_name] = [(time, artist) for time, artist in schedule[venue_name][stage_name] if time != time_slot]
         schedule[venue_name][stage_name].append((time_slot, artist_name))
-        print(f"Succesfully change artist in slot {time_slot}")
+        print(f"\nSuccesfully change artist in slot {time_slot}")
         return True
     else:
-        print("Error: Venue or stage does not exist")
+        print("\nError: Venue or stage does not exist")
         return False
 
 def display_schedule(time_slot_list: tuple, schedule: dict):
+    print("")
     if not schedule:
         print("Schedule is empty")
     else:
@@ -107,19 +108,39 @@ def display_schedule(time_slot_list: tuple, schedule: dict):
             for stage in schedule[venue].keys():
                 print(f"   Stage | {stage}:")
                 for slot in time_slot_list:
-                    print(f"{slot} : ", end="")
+                    print(f"      {slot} : ", end="")
                     for (time, artist) in schedule[venue][stage]:
                         if time == slot:
                             print(f"{artist}")
                         else:
                             print("NONE")
+                    print("")
+
+def display_stage_schedule(time_slot_list: tuple, schedule: dict, venue: str):
+    print("")
+    if not schedule:
+        print("Schedule is empty")
+    else:
+        for stage in schedule[venue].keys():
+            print(f"   Stage | {stage}:")
+            for slot in time_slot_list:
+                print(f"      {slot} : ", end="")
+                for (time, artist) in schedule[venue][stage]:
+                    if time == slot:
+                        print(f"{artist}")
+                    else:
+                        print("NONE")
+                print("")
 
 def display_stage(venues: dict, venue: str):
-    print("stage : ")
+    print("")
     for stage in venues[venue].keys():
-        print(f"{stage}")
+        print(f"Stage | {stage}:")
+        print(f"   Location: {venues[venue][stage]['location']}")
+        print(f"   Equipment: {', '.join(venues[venue][stage]['equipment']) if venues[venue][stage]['equipment'] else 'None'}")
 
 def display_venues(venues: dict):
+    print("")
     for venue in venues.keys():
         print(f"Venue | {venue}:")
         for stage in venues[venue].keys():
@@ -184,7 +205,7 @@ def main():
 
                         show_venues(venues)
 
-                        venue = input("Enter venue to modified\n>>> ")
+                        venue = input("\nEnter venue to modified\n>>> ")
 
                         if venue in venues:
                             while True:
@@ -252,7 +273,7 @@ def main():
 
                                                 case '2':
                                                     #Assign Artist
-                                                    display_stage(venues, venue)
+                                                    display_stage_schedule(time_slot_list, schedule, venue)
                                                     stage_name = input("Enter stage name: ")
                                                     for time in time_slot_list:
                                                         print(f"| {time} ", end="")
@@ -263,7 +284,7 @@ def main():
 
                                                 case '3':
                                                     #Remove Time Slot
-                                                    display_stage(venues, venue)
+                                                    display_stage_schedule(time_slot_list, schedule, venue)
                                                     stage_name = input("Enter stage name: ")
                                                     time_slot = input("Enter time slot to remove: ")
 
@@ -271,7 +292,7 @@ def main():
                                                     
                                                 case '4':
                                                     #Change Artist
-                                                    display_stage(venues, venue)
+                                                    display_stage_schedule(time_slot_list, schedule, venue)
                                                     stage_name = input("Enter stage name: ")
                                                     time_slot = input("Enter time slot: ")
                                                     artist_name = input("Enter new artist name: ")      
